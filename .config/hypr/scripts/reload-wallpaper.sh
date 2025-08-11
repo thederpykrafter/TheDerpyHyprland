85288217bf5a917929479fa696c94998b66d2509
thederpykrafter <thederpykrafter@gmail.com>
2025-08-11 17:29:09 -0400

    update wallpaper animation

--- .config/hypr/scripts/reload-wallpaper.sh
+++ .config/hypr/scripts/reload-wallpaper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 swww clear
-swww img ~/.config/hypr/wallpapers/wallpaper1.jpg --transition-type grow --transition-duration 1
+swww img ~/.config/hypr/wallpapers/wallpaper1.jpg --transition-type grow --transition-duration 0.7
