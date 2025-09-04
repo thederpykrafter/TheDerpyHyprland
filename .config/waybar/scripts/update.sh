142ff435f90f15e941e8a5061369652825103be0
thederpykrafter <thederpykrafter@gmail.com>
2025-09-03 21:23:56 -0400

    use pacman if aur is down

--- .config/waybar/scripts/update.sh
+++ .config/waybar/scripts/update.sh
@@ -2,13 +2,13 @@
 
 if [[ $(command -v toilet) ]] && [[ $(command -v lolcat) ]]; then
   toilet -f future "yay -Syu" | lolcat
-  yay --noconfirm
+  yay --noconfirm || sudo pacman -Syu
 
   toilet -f future "Press any key to continue..." | lolcat
   read -n 1
 else
   echo "yay -Syu"
-  yay --noconfirm
+  yay --noconfirm || sudo pacman -Syu
 
   read -p "Press any key to continue..." -n 1
 fi
